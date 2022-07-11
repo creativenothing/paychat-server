c3f27b0a1e3c57f30d0d04c127d785e375f1f125
Jack <jack@anarchyplanet.org>
2022-07-11 02:38:35 +0000

    Add websocket endpoint for chat room

--- router/router.go
+++ router/router.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/creativenothing/paychat-server/controllers"
 	"github.com/creativenothing/paychat-server/sessions"
+	"github.com/creativenothing/paychat-server/websocket"
 	"github.com/gorilla/mux"
 )
 
@@ -92,18 +93,64 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(usersession.UserResponse())
 }
 
+func wsChatroom(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	usersession := sessions.GetUserSession(w, r)
+
+	fmt.Printf("Chat session with %v and %v\n", vars, usersession)
+
+	hub := websocket.GetHub("chat", id)
+	var handler websocket.ClientHandler = func(c *websocket.Client, message []byte) {
+		fmt.Printf("Chat message with %v and \n%s\n", usersession, string(message))
+		messageJSON := map[string]interface{}{}
+		if err := json.Unmarshal(message, &messageJSON); err != nil {
+			return
+		}
+
+		if _, ok := messageJSON["type"].(string); !ok {
+			return
+		}
+		switch messageJSON["type"].(string) {
+		case "chat":
+			// Ensure well formed
+			if _, ok := messageJSON["text"].(string); !ok {
+				return
+			}
+			text := messageJSON["text"].(string)
+
+			chat := controllers.NewChatMessage("chat", text, usersession.UserID)
+
+			send, _ := json.Marshal(map[string]interface{}{
+				"type":   "chat",
+				"id":     chat.ID,
+				"userid": usersession.UserID,
+				"text":   text,
+			})
+
+			c.Broadcast([]byte(send))
+			break
+		}
+
+	}
+
+	websocket.ServeWsWithHandler(hub, w, r, handler)
+}
+
 func SetupRouter() {
 	Router = mux.NewRouter()
 	Router.Use(cors)
 
 	Router.HandleFunc("/", preflight).Methods("OPTIONS")
-	Router.HandleFunc("/", home)
 
 	Router.HandleFunc("/logout", logout).Methods("GET")
 	Router.HandleFunc("/login", login).Methods("POST")
 	Router.HandleFunc("/signup", controllers.RegisterUser).Methods("POST")
 	Router.HandleFunc("/user/{id}", controllers.GetUser).Methods("GET")
 	Router.HandleFunc("/user", controllers.GetAllUsers).Methods("GET")
+	Router.HandleFunc("/chat/ws", wsChatroom).Methods("GET")
 	Router.HandleFunc("/auth", auth).Methods("GET")
 
+	Router.HandleFunc("/", home)
 }
